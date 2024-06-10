29c4150bfb6e37496ad582c4dddbfca34a0cb778
nicomiguelino <nicomiguelino2014@gmail.com>
2024-06-10 14:12:28 -0700

    update hashes

--- bin/deploy_to_balena.sh
+++ bin/deploy_to_balena.sh
@@ -73,7 +73,7 @@ if [[ -z "${FLEET+x}" ]]; then
     exit 1
 fi
 
-export GIT_SHORT_HASH="1ee5526"
+export GIT_SHORT_HASH="e062f59"
 export DEFAULT_SHM_SIZE='256mb'
 
 if [[ -z "${SHM_SIZE+x}" ]]; then
