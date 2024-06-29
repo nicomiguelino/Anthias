6b91170acc460386c9a8ec9c5616d4d4a4d6269a
nicomiguelino <nicomiguelino2014@gmail.com>
2024-06-28 17:35:20 -0700

    test: prepare the changes for testing

--- bin/deploy_to_balena.sh
+++ bin/deploy_to_balena.sh
@@ -73,7 +73,7 @@ if [[ -z "${FLEET+x}" ]]; then
     exit 1
 fi
 
-export GIT_SHORT_HASH=${GIT_SHORT_HASH:-latest}
+export GIT_SHORT_HASH="e3e6e31"
 export DEFAULT_SHM_SIZE='256mb'
 
 if [[ -z "${SHM_SIZE+x}" ]]; then
